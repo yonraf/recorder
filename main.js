0c8f00bb254cf7339de1ef54e7d032a5cee2cc4e
yonraf <55747457+yonraf@users.noreply.github.com>
2024-03-08 15:34:47 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,59 @@
+const mic_btn = document.querySelector('#mic');
+const playback = document.querySelector('.playback');
+
+mic_btn.addEventListener('click', ToggleMic);
+
+let can_record = false;
+let is_recording = false;
+
+let recorder = null;
+let chunks = [];
+
+function SetupAudio() {
+    console.log("Setup");
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+            .getUserMedia({
+                audio: true
+            })
+            .then(SetupStream)
+            .catch(err => {
+                console.error(err)
+            }
+
+            )
+    }
+}
+
+SetupAudio();
+
+function SetupStream(stream) {
+    recorder = new MediaRecorder(stream);
+
+    recorder.ondataavailable = e => {
+        chunks.push(e.data)
+    }
+
+    recorder.onstop = e => {
+        const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus"});
+        chunks = [];
+        const audioURL = window.URL.createObjectURL(blob);
+        playback.src = audioURL;
+    }
+
+    can_record = true;
+}
+
+function ToggleMic() {
+    if(!can_record) return;
+
+    is_recording = !is_recording;
+
+    if(is_recording) {
+        recorder.start();
+        mic_btn.classList.add("is-recording")
+    } else {
+        recorder.stop();
+        mic_btn.classList.remove("is-recording")
+    }
+}
